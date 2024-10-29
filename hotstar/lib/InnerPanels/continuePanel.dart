f06347bb8eecc2ac3d2ef06346c1bfd1ee0f84ba
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-29 16:10:59 +0530

    Lets find another way to handle it

--- hotstar/lib/InnerPanels/continuePanel.dart
+++ hotstar/lib/InnerPanels/continuePanel.dart
@@ -21,7 +21,6 @@ class _ContinuePanelState extends State<ContinuePanel> {
         children: List.generate(4, (i) {
           return Padding(padding: EdgeInsets.only(left: 5),
           child: Container(
-            color: Colors.green,
             height: 144,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
@@ -30,12 +29,18 @@ class _ContinuePanelState extends State<ContinuePanel> {
                 Container(
                   width: 182,
                   height: 100,
-                  child: DecoratedBox(decoration: BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5))),),
+                  child: DecoratedBox(decoration: BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5)),
+                      
+                    ),
+                    child: ClipRRect(
+                      child: Image.asset('images/aven.jpeg', fit: BoxFit.cover,),
+                    ),
+                  ),
                 ),
                 Container(
                   width: 182,
                   height: 44,
-                  
+                  child: DecoratedBox(decoration: BoxDecoration(borderRadius: BorderRadius.only(bottomLeft: Radius.circular(5), bottomRight: Radius.circular(5)),color: Color(0xFF0F1014))),
                 )     
               ],
             ),
