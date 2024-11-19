1d1ae26887acca042f83a3befa7f13ce5e5bca3e
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-11-19 14:22:19 +0530

    need to fix the Dynamic list, now launch break

--- hotstar/lib/NaviBarPage/NewPage/newsExperience.dart
+++ hotstar/lib/NaviBarPage/NewPage/newsExperience.dart
@@ -8,10 +8,76 @@ class NewsExperience extends StatefulWidget {
 }
 
 class _NewsExperienceState extends State<NewsExperience> {
+
+  final List<dynamic> items = [
+    "English",
+    "Drama",
+    "Fantasy",
+    "Crime",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
-     color: Colors.red, 
+      margin: EdgeInsets.only(top: 70),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              height: 225,
+              child: ClipRRect(
+                child: Image.asset("images/spiderman.jpg", fit: BoxFit.cover,),
+              ), 
+            ),
+            Container(
+              width: double.infinity,
+              height: 210,
+              color: Color(0xFF141820),
+              child:Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start, 
+                  children: [
+                    Container(
+                      width: 150,
+                      height: 72,
+                      child: ClipRRect(
+                        child: Image.asset("images/spidertitle.png"),
+                      ), 
+                    ),
+                    Text("Releasing on Nov 19", style: TextStyle(color: Colors.white, fontSize: 10.5, fontWeight: FontWeight.w600),),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: items.asMap().entries.map((item) {
+
+                        int index = item.key;
+                        Map<String, dynamic> value = item.value;
+
+                          return Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 5),
+                                child: Text(value, style: TextStyle(color: Colors.white),),
+                              ),
+                              Text(".", style: TextStyle(color: Color(0xFF959EA9), fontSize: 10.5),)
+                            ],
+                          );
+                      }).toList(),
+                    ),
+                  ], 
+                ),
+              ) ,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
